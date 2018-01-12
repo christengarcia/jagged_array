68e7492c411529f9f88a1dd5e530d0e1fe38ccab
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-12 15:10:58 +1100

    Initial commit

---
+++ jagged_array/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * C# Program to Find the Length of the Jagged Array using Predefined Functions
+ */
+
+using System;
+
+namespace jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[][] numbers = new byte[5][];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = new byte[i + 3];
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Length of row {0} is {1}", i, numbers[i].Length);
+            }
+            Console.Read();
+        }
+    }
+}
